3875aa0e833b624f45aad45c7a45a95033822be4
wulingren <wulingren@pinduoduo.con>
2023-04-04 20:05:01 +0800

    feature:change the title

--- utils/global-data.js
+++ utils/global-data.js
@@ -4,7 +4,7 @@ export const getGlobalData = () => {
     : 'Will Li';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
-    : 'Next.js Blog Theme';
+    : 'Learn English! Try Harder!';
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
     : 'All rights reserved.';
